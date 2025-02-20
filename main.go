3efdba8bf07ecf8ee9590fd4696bd746a95cb0e8
v-gira <virgile.girardin@gmail.com>
2025-02-20 16:14:59 +0100

    feat: ch5 l5

--- main.go
+++ main.go
@@ -12,16 +12,26 @@ import (
 )
 
 type apiConfig struct {
-	fileserverHits atomic.Int32
-	db      *database.Queries
+	fileserverHits 	atomic.Int32
+	db      		*database.Queries
+	platform 		string
 }
 
 func main() {
-	godotenv.Load()
-
 	const filepathRoot = "."
 	const port = "8080"
+
+	godotenv.Load()
+
 	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL must be set")
+	}
+
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
 
 	db, err := sql.Open("postgres", dbURL)
 	if err != nil {
@@ -33,12 +43,14 @@ func main() {
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		db : queries,
+		platform: platform,
 	}
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerUsersCreate)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
--- reset.go
+++ reset.go
@@ -3,7 +3,13 @@ package main
 import "net/http"
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	if cfg.platform != "dev" {
+		w.WriteHeader(http.StatusForbidden)
+		w.Write([]byte("Only admin platform can reset hits"))
+		return
+	}
+	cfg.db.Reset(r.Context())
 	cfg.fileserverHits.Store(0)
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Hits reset to 0"))
+	w.Write([]byte("Hits reset to 0, database reset"))
 }
